9626c80fc3b554e6efcd091d9090ace3eeeb255a
KostyantynVegas <41579834+KostyantynVegas@users.noreply.github.com>
2018-07-24 00:36:02 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ const bot = new Discord.Client();
 console.log("BotWorks");//наличие багов.
 
 //bot register.
-bot.login("NDcwMjU2MTc5NzgwNjQ4OTcx.DjYCwg.S-zss2_7QtOhxkWeXiitpnHynJc");
+bot.login(token);
 bot.login(process.env.BOT_KEYFORROBLOX);
 
 //start code.
 //help commands.
 bot.on('message', message => {
      if (message.content ==="#help") {
-         message.reply("Commands:!credits !noob, !lol, !shytka, !myavatar, !meme.");
+         message.reply("Commands:!credits, !message, !puzzle, !my avatar, !meme.");
      }
 });
 //credits command.
@@ -21,28 +21,34 @@ bot.on('message', message => {
         message.reply("Programmed on J.S. by Roenko Kostyantyn.");
     }
 });
-//noob command.
+//random message.
+//variables.
 bot.on('message', message => {
-    if (message.content ==="!noob") {
-        message.reply("no! u noob too!!!");
-    }
-});
-//lol command.
-bot.on('message', message => {
-    if (message.content ==="!lol") {
-        message.reply("lol");
-    }
-});
-//random shytka.
-bot.on('message', message => {
-    if (message.content ==="!shytka") {
-        message.reply("Однажды я гулял по лесу, вижу колодец. Подхожу к нему, захожу в лифт, сажусь на велосипед, завожу мотоцикл, тут бац, ко мне кондуктор подходит и говорит: ''Дядя, уступи место''. А я говорю что не курю. Так этот ребенок как начнет плакать! Но я не растерялся, и как дал ему по роже, а он хватается за коленку и кричит: ''Моя спина! Моя спина!''. Выхожу я из этой электрички, смотрю, маршрутка стоит. Ну подхожу и говорю: ''Винстон синий и водки 0,5''. Ну она мне и дает Bond и пиво. Я беру эту приму и самогон, и убегаю от этих мусоров! Так эти пожарники меня на скорой догнали! Догнали и говорят: ''Вы сдачу забыли!''. Ну я беру килограмм яблок, и иду на базар торговать. Приношу, не успел выложить эти бананы, тут же бабка подбегает и скупает у меня все персики, и говорит чтобы все сложил в кулечек! А я думаю, что она совсем больная, как я ей 20 арбузов в сумку положу?! Взял я деньги и пошел домой! Больше я в лес не ходил... и грибы не трогал... даже курить бросил.");
-    }
+var joke1 = "Однажды я гулял по лесу, вижу колодец. Подхожу к нему, захожу в лифт, сажусь на велосипед, завожу мотоцикл, тут бац, ко мне кондуктор подходит и говорит: ''Дядя, уступи место''. А я говорю что не курю. Так этот ребенок как начнет плакать! Но я не растерялся, и как дал ему по роже, а он хватается за коленку и кричит: ''Моя спина! Моя спина!''. Выхожу я из этой электрички, смотрю, маршрутка стоит. Ну подхожу и говорю: ''Винстон синий и водки 0,5''. Ну она мне и дает Bond и пиво. Я беру эту приму и самогон, и убегаю от этих мусоров! Так эти пожарники меня на скорой догнали! Догнали и говорят: ''Вы сдачу забыли!''. Ну я беру килограмм яблок, и иду на базар торговать. Приношу, не успел выложить эти бананы, тут же бабка подбегает и скупает у меня все персики, и говорит чтобы все сложил в кулечек! А я думаю, что она совсем больная, как я ей 20 арбузов в сумку положу?! Взял я деньги и пошел домой! Больше я в лес не ходил... и грибы не трогал... даже курить бросил.";
+var joke2 = "Что посеешь, то и пожнёшь.";
+var joke3 = "Привет...";
+var joke4 = "что??";
+var joke5 = "message.";
+var joke6 = "Го играть?";
+var joke7 = "Удачного дня!";
+var numbers = 7;
+var randomizer = Math.floor(Math.random() * (numbers -1 +1)) +1;
+    if (message.content ==="!message") {
+    switch (randomizer) {
+     case 1: message.reply([joke1]); break;
+     case 2: message.reply([joke2]); break;
+     case 3: message.reply([joke3]); break;
+     case 4: message.reply([joke4]); break;
+     case 5: message.reply([joke5]); break;
+     case 6: message.reply([joke6]); break;
+     case 7: message.reply([joke7]); break;
+    }   
+}
 });
 //Author image avatar.
 bot.on("message", message => {
     var avatar = message.author.avatarURL;
-   if (message.content ==="!myavatar") {
+   if (message.content ==="!my avatar") {
        message.reply("Your avatar is, " + avatar);
    }
 });
@@ -56,6 +62,7 @@ var img4="https://orig00.deviantart.net/b00d/f/2016/118/9/b/triggered_by_yaycoco
 var img5="https://i.kym-cdn.com/photos/images/newsfeed/000/170/791/welcome-to-the-internet-internet-demotivational-poster-1264714433.png.jpg";
 var img6="https://i0.wp.com/ramenswag.com/wp-content/uploads/2016/09/14442632_1263969750313763_451636988_n-300x274.jpg?resize=300%2C274";
 var img7="https://i.imgflip.com/29lhcg.jpg";
+var token = "NDcwMjU2MTc5NzgwNjQ4OTcx.DjYCwg.S-zss2_7QtOhxkWeXiitpnHynJc";
 var img8="https://memepedia.ru/wp-content/uploads/2016/08/chanti-binks-mem.jpg";
 var img9="https://pics.me.me/me-condoms-please-cashier-31659952.png";
 var img10="http://www.dictionary.com/e/wp-content/uploads/2018/03/dank-meme.jpg";
@@ -82,4 +89,57 @@ var random = Math.floor(Math.random() * (number - 1 +1)) +1;
      }
  }
 });
+//random puzzle.
+//variables.
+bot.on('message', message => {
+    var p1 = "Без ног и без крыльев оно,Быстро летит, не догонишь его. Ответ писать через '!' ";
+    var p2 = "Тебе дано,А люди им пользуются. Ответ писать через '!' ";
+    var p3 = "Одной ручкой всех встречает,Другой ручкой провожает. Ответ писать через '!' ";
+    var p4 = "Себя он раскрывает, тебя он закрывает,Только дождичек пройдет — сделает наоборот. Ответ писать через '!' ";
+    var p5 = "Синие покрывало-весь мир покрыла. Ответ писать через '!' ";
+    var numbers = 5;
+    var randomizer = Math.floor(Math.random() * (numbers -1 +1)) +1;
+        if (message.content ==="!puzzle") {
+        switch (randomizer) {
+         case 1: message.reply([p1]); break;
+         case 2: message.reply([p2]); break;
+         case 3: message.reply([p3]); break;
+         case 4: message.reply([p4]); break;
+         case 5: message.reply([p5]); break;
+        }   
+    }
+    });
+
+//Ответы на загадки. время,имя,дверь,зонт,небо.
+//time.
+bot.on('message', message => {
+    if (message.content ==="!время") {
+        message.reply("Great!Без ног и без крыльев оно,Быстро летит, не догонишь его. Ответ: время.");
+    }
+});
+//name.
+bot.on('message', message => {
+if (message.content ==="!имя") {
+    message.reply("Good job. Тебе дано,А люди им пользуются. Ответ: имя.");
+}
+});
+//door.
+bot.on('message', message => {
+if (message.content ==="!дверь") {
+    message.reply("Normal. Одной ручкой всех встречает,Другой ручкой провожает. Ответ: дверь.");
+}
+});
+//parasoll.
+bot.on('message', message => {
+if (message.content ==="!зонт") {
+    message.reply("Easy. Себя он раскрывает, тебя он закрывает,Только дождичек пройдет — сделает наоборот. Ответ: зонт.");
+}
+});
+//sky.
+bot.on('message', message => {
+if (message.content ==="!небо") {
+    message.reply("Noob. Синие покрывало-весь мир покрыла. Ответ: небо.");
+}
+});
+
 //end. :P
